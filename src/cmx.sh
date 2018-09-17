ea118e7a6040b67f31c77de9f01e1c1d6aba17d4
Mario Catuogno <mario.catuogno@gmail.com>
2018-09-17 15:19:29 +0200

    Change script path

--- src/cmx.sh
+++ src/cmx.sh
@@ -2,13 +2,13 @@
 
 case "$1" in  
     install|INSTALL)
-        ~/Dropbox/Github/Clean-macOS/bin/cmx_install.sh
+        ~/Dropbox/Github/Clean-macOS/src/cmx_install.sh
         ;;
     config|CONFIG)
-        ~/Dropbox/Github/Clean-macOS/bin/cmx_config.sh
+        ~/Dropbox/Github/Clean-macOS/src/cmx_config.sh
         ;;   
     update|UPDATE)
-        ~/Dropbox/Github/Clean-macOS/bin/cmx_update.sh
+        ~/Dropbox/Github/Clean-macOS/src/cmx_update.sh
         ;;
     *)
 		echo "Usage: $0 [ install | config | update ]" ; exit 1
