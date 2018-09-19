0e0dda47bbb4ea529cd3c99c611f1e00fc90638c
Mario Catuogno <mario.catuogno@gmail.com>
2018-09-19 23:50:09 +0200

    Update cmx_update script

--- src/cmx_update.sh
+++ src/cmx_update.sh
@@ -16,16 +16,13 @@ while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 # Configuration                                                               #
 ###############################################################################
 
-printf "Sublime Text: update preferences\n"
-curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/subl.settings -o ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-printf "Sublime Text: update packages\n"
-curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/sublpkg.settings -o ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Package\ Control.sublime-settings
 printf "Visual Studio Code: update preferences\n"
-curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/vscode.settings -o ~/Library/Application\ Support/Code/User/settings.json
+curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/settings.json -o ~/Library/Application\ Support/Code/User/settings.json
 
 ###############################################################################
 # Update                                                                      #
 ###############################################################################
+
 mas upgrade
 brew upgrade && brew update && brew cleanup && brew cask cleanup && brew prune
 brew doctor
