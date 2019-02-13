74d36b7a258ff9c5caf2cf788c1a6c6f571de439
Mario Catuogno <mario.catuogno@gmail.com>
2019-02-13 22:27:42 +0100

    Overhaul of Clean-macOS scripts

--- bin/update.sh
+++ bin/update.sh
@@ -1,43 +1,43 @@
 #!/bin/bash
 
-# NAME: Clean macOS script
-# DESC: Update brews, formulas and apps configurations and preferences
-# DATE: 26-09-2018
-# VERSION: 1.5.0
+# NAME: Clean macOS UPDATE script
+# DESC: Update script for clean macOS
+# DATE: 13-02-2019
+# VERSION: 1.6.0
 
 ###############################################################################
 # Launch script                                                               #
 ###############################################################################
 
 #Entering as Root
-echo "Enter root password..."
+printf "Enter root password...\n"
 sudo -v
 
 #Keep alive Root
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 ###############################################################################
-# Configuration                                                               #
-###############################################################################
-
-#Git
-printf "Git: update preferences\n"
-curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/.gitignore -o ~/.gitignore
-
-#Visual Studio Code
-printf "Visual Studio Code: update preferences\n"
-curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/settings.json -o ~/Library/Application\ Support/Code/User/settings.json
-
-#ZSH
-printf "ZSH: update preferences\n"
-curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/.zshrc -o ~/.zshrc
-
-###############################################################################
 # Update                                                                      #
 ###############################################################################
 
-brew update && brew upgrade && brew cleanup && brew cleanup && brew doctor
-mas upgrade
+brew update && brew upgrade && brew cleanup && brew doctor && mas upgrade
+
+###############################################################################
+# Config                                                                      #
+###############################################################################
+
+#Download GIT settings
+printf "Git: update preferences\n"
+curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/.gitignore -o ~/.gitignore
+curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/.gitconfig -o ~/.gitconfig
+
+#Download Visual Studio Code settings
+printf "Visual Studio Code: update preferences\n"
+curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/settings.json -o ~/Library/Application\ Support/Code/User/settings.json
+
+#Download ZSH settings
+printf "ZSH: update preferences\n"
+curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/.zshrc -o ~/.zshrc
 
 #Exit script
 exit
