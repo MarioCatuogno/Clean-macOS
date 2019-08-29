a3d8fe446c36668c140467c93802970f32803e71
Mario Catuogno <mario.catuogno@gmail.com>
2019-08-29 15:13:41 +0200

    Update personal.sh (#56)

--- bin/personal.sh
+++ bin/personal.sh
@@ -61,6 +61,9 @@ brew install youtube-dl
 ###############################################################################
 
 # 🛠️ Developer Tools
+printf "🛠️  Install Docker.app...\n"
+brew cask install --appdir="/Applications" docker
+
 printf "🛠️  Install iTerm2.app...\n"
 brew cask install --appdir="/Applications" iterm2
 
@@ -71,16 +74,28 @@ brew cask install --appdir="/Applications" visual-studio-code
 printf "🍿  Install Handbrake.app...\n"
 brew cask install --appdir="/Applications" handbrake
 
+printf "🍿  Install Helium.app...\n"
+mas install 1054607607
+
 printf "🍿  Install Iina.app...\n"
 brew cask install --appdir="/Applications" iina
 
 # 🎲 Games
+printf "🎲  Install Battle Net.app...\n"
+brew cask install --appdir="/Applications" battle-net
+
 printf "🎲  Install Steam.app...\n"
 brew cask install --appdir="/Applications" steam
+
+printf "🎲  Install Steermouse.app...\n"
+brew cask install --appdir="/Applications" steermouse
 
 # 🏞️ Graphics & Design
 printf "🏞️  Install Imageoptim.app...\n"
 brew cask install --appdir="/Applications" imageoptim
+
+printf "🏞️  Install Pixelmator.app...\n"
+mas install 407963104
 
 # 🧭 Navigation
 printf "🧭  Install Ecosia.app...\n"
@@ -109,6 +124,11 @@ printf "📝  Install Spark.app...\n"
 mas install 1176895641
 
 # 🔑 Security
+printf "🔑  Install Expressvpn.app...\n"
+brew cask install --appdir="/Applications" expressvpn
+
+printf "🔑  Install Wipr.app...\n"
+mas install 1320666476
 
 # 🥳 Social
 printf "🥳  Install Franz.app...\n"
@@ -126,6 +146,12 @@ brew cask install --appdir="/Applications" appcleaner
 
 printf "🧮  Install Cheatsheet.app...\n"
 brew cask install --appdir="/Applications" cheatsheet
+
+printf "🧮  Install DaisyDisk.app...\n"
+mas install 411643860
+
+printf "🧮  Install Magnet.app...\n"
+mas install 441258766
 
 printf "🧮  Install The Unarchiever.app...\n"
 mas install 425424353
@@ -230,3 +256,23 @@ brew cask install qlstephen
 brew cask install qlvideo
 brew cask install quicklook-csv
 brew cask install quicklook-json
+
+###############################################################################
+# Install Python                                                              #
+###############################################################################
+
+# ⚙️ Install Python [1/2]
+printf "⚙️  Install Python3...\n"
+brew install python3
+
+# ⚙️ Download Python packages [2/2]
+printf "⚙️  Download Python packages...\n"
+pip3 install autopep8
+pip3 install jupyter
+pip3 install numpy
+pip3 install organize-tool
+pip3 install pandas
+pip3 install pylint
+pip3 install requests
+pip3 install speedtest-cli
+pip3 install virtualenv
