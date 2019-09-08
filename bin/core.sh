e7e51ac9770f3aa2435bc7c67a2ebc636a3c1472
Mario Catuogno <mario.catuogno@gmail.com>
2019-09-08 23:15:21 +0200

    Update core.sh (#36)

--- bin/core.sh
+++ bin/core.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
-# SCRIPT: XXX
-# DATE: AAAA-MM-DD
-# VERSION: X.X.X
+# DATE: 2019-10-31
+# VERSION: 1.8.0
 
 ###############################################################################
 # Install HomeBrew                                                            #
@@ -35,6 +34,8 @@ brew tap homebrew/cask-fonts
 printf "⚙️  Install Binaries...\n"
 brew install ack
 brew install bash
+brew install coreutils
+brew install dockutil
 brew install gzip
 brew install htop
 brew install imagemagick
@@ -117,8 +118,6 @@ mas install 409201541
 # 🔑 Security
 
 # 🥳 Social
-printf "🥳  Install Franz.app...\n"
-brew cask install --appdir="/Applications" franz
 
 # 🧮 Utilities
 printf "🧮  Install Alfred.app...\n"
@@ -137,7 +136,7 @@ printf "🧮  Install The Unarchiever.app...\n"
 mas install 425424353
 
 ###############################################################################
-# Install Dotfiles                                                            #
+# Configure Terminal                                                          #
 ###############################################################################
 
 # 🎛️ Install Git [1/2]
@@ -167,3 +166,33 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 printf "🎛️  Update Zsh settings...\n"
 sudo rm -rf ~/.zshrc > /dev/null 2>&1
 curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/.zshrc -o ~/.zshrc
+
+###############################################################################
+# Install Config files                                                        #
+###############################################################################
+
+# 🎛️ Download iTerm2 plugins [1/1]
+printf "🎛️  Download iTerm2 plugins...\n"
+curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/ayu-dark.itermcolors -o ~/Downloads/ayu-dark.itermcolors && open ~/Downloads/ayu-dark.itermcolors
+curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/ayu-light.itermcolors -o ~/Downloads/ayu-light.itermcolors && open ~/Downloads/ayu-light.itermcolors
+curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/ayu-mirage.itermcolors -o ~/Downloads/ayu-mirage.itermcolors && open ~/Downloads/ayu-mirage.itermcolors
+
+# 🎛️ Download Visual Studio Code plugins [1/2]
+printf "🎛️  Download Visual Studio Code plugins...\n"
+code --install-extension 77qingliu.sas-syntax
+code --install-extension DavidAnson.vscode-markdownlint
+code --install-extension GrapeCity.gc-excelviewer
+code --install-extension HookyQR.beautify
+code --install-extension JaimeOlivares.yuml
+code --install-extension Tyriar.sort-lines
+code --install-extension formulahendry.code-runner
+code --install-extension mikestead.dotenv
+code --install-extension ms-python.python
+code --install-extension pnp.polacode
+code --install-extension teabyii.ayu
+code --install-extension yzhang.markdown-all-in-one
+
+# 🎛️ Update Visual Studio Code settings [2/2]
+printf "🎛️  Update Visual Studio Code settings...\n"
+sudo rm -rf ~/Library/Application\ Support/Code/User/settings.json > /dev/null 2>&1
+curl https://raw.githubusercontent.com/MarioCatuogno/Clean-macOS/master/config/settings.json -o ~/Library/Application\ Support/Code/User/settings.json
