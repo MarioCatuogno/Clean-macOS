87c6c06a8bbb19213bd6f4fd5e93727d1426ba22
Mario Catuogno <mario.catuogno@gmail.com>
2019-12-03 22:41:58 +0100

    Update cleanMacOs.sh

--- bin/cleanMacOs.sh
+++ bin/cleanMacOs.sh
@@ -13,3 +13,10 @@ sudo -v
 
 # Keep alive Root
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+#!/bin/bash
+while IFS="" read -r p || [ -n "$p" ]
+do
+  printf '%s\n' "Installing $p..."
+  printf '%s\n' "Ciao!"
+done < /Users/mariocatuogno/Desktop/testo.txt
