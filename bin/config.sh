74638dd4f5e02a1c7285c2fb5c0da36becbeed19
Mario Catuogno <mario.catuogno@gmail.com>
2020-08-05 15:26:34 +0200

    Update config.sh

--- bin/config.sh
+++ bin/config.sh
@@ -88,11 +88,11 @@ cp $CONFIG/.gitconfig ~/.gitconfig
 # Install Miniconda                                                           #
 ###############################################################################
 
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
-zsh ~/miniconda.sh -b -p $HOME/miniconda
-source $HOME/miniconda/bin/activate
-conda init zsh
-conda deactivate
+# wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -O ~/miniconda.sh
+# zsh ~/miniconda.sh -b -p $HOME/miniconda
+# source $HOME/miniconda/bin/activate
+# conda init zsh
+# conda deactivate
 
 ###############################################################################
 # Configure macOS: Dock                                                       #
