b02062cc3ccb14f2a732a24e4ec3b4043b9a5ec1
Mario Catuogno <mario.catuogno@gmail.com>
2020-08-10 23:30:35 +0200

    Update config.sh (#79)

--- bin/config.sh
+++ bin/config.sh
@@ -82,6 +82,54 @@ cp $CONFIG/.gitignore ~/.gitignore
 cp $CONFIG/.gitconfig ~/.gitconfig
 
 ###############################################################################
+# Configure macOS                                                             #
+###############################################################################
+
+printf "⚙️ Configure Finder...\n"
+defaults write -g AppleShowAllExtensions -bool true
+defaults write com.apple.finder AppleShowAllFiles true
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+defaults write com.apple.finder ShowPathbar -bool true
+chflags nohidden ~/Library
+/System/Library/Frameworks/CoreServices.framework/Frameworks/LaunchServices.framework/Support/lsregister -kill -r -domain local -domain system -domain user
+
+printf "⚙️ Save screenshots in PNG format...\n"
+mkdir ~/Pictures/Screenshots
+defaults write com.apple.screencapture location -string "~/Pictures/Screenshots"
+defaults write com.apple.screencapture type -string "png"
+
+printf "⚙️ Configure Keyboard...\n"
+defaults write -g NSAutomaticSpellingCorrectionEnabled -bool false
+defaults write -g NSAutomaticPeriodSubstitutionEnabled -bool false
+defaults write -g NSAutomaticDashSubstitutionEnabled -bool false
+defaults write -g NSAutomaticQuoteSubstitutionEnabled -bool false
+defaults write com.apple.messageshelper.MessageController SOInputLineSettings -dict-add "continuousSpellCheckingEnabled" -bool false
+
+printf "⚙️ Configure Safari...\n"
+defaults write com.apple.Safari UniversalSearchEnabled -bool false
+defaults write com.apple.Safari SuppressSearchSuggestions -bool true
+defaults write com.apple.Safari WebAutomaticSpellingCorrectionEnabled -bool false
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+defaults write com.apple.Safari ShowFavoritesBar -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+
+printf "⚙️ Configure TextEdit...\n"
+defaults write com.apple.TextEdit RichText -int 0
+defaults write com.apple.TextEdit PlainTextEncoding -int 4
+defaults write com.apple.TextEdit PlainTextEncodingForWrite -int 4
+
+printf "⚙️ Configure Trackpad...\n"
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false
+
+printf "⚙️ Various configuration...\n"
+defaults write com.apple.gamed Disabled -bool true
+sudo /usr/libexec/ApplicationFirewall/socketfilterfw --setglobalstate on
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
+###############################################################################
 # Final touches                                                               #
 ###############################################################################
 
