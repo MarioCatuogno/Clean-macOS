83c2f356100e73c1421fbf25511c45f06d4257c3
Mario Catuogno <mario.catuogno@gmail.com>
2021-04-07 23:31:55 +0200

    Update config.sh

--- bin/config.sh
+++ bin/config.sh
@@ -53,6 +53,7 @@ code --install-extension file-icons.file-icons
 code --install-extension formulahendry.code-runner
 code --install-extension HookyQR.beautify
 code --install-extension ivangabriele.vscode-git-add-and-commit
+code --install-extension knisterpeter.vscode-github
 code --install-extension mikestead.dotenv
 code --install-extension ms-python.python
 code --install-extension teabyii.ayu
