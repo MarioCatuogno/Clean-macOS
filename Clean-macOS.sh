fc0b914980619210503796aa656509a9c8645a6d
Mario Catuogno <mario.catuogno@gmail.com>
2023-02-28 14:30:26 +0100

    Optimize Clean-macOS.sh

--- Clean-macOS.sh
+++ Clean-macOS.sh
@@ -1,42 +1,58 @@
 #!/bin/bash
 
+set -e
+
 ###############################################################################
 # Set variables                                                               #
 ###############################################################################
 
-BIN=~/Clean-macOS/bin                # shell scripts
-CONFIG=~/Clean-macOS/config          # configuration files directory
-SETUP=~/Clean-macOS                  # root folder of Clean-macOS
+BIN="${HOME}/Clean-macOS/bin"                # shell scripts
+CONFIG="${HOME}/Clean-macOS/config"          # configuration files directory
+SETUP="${HOME}/Clean-macOS"                  # root folder of Clean-macOS
 
 ###############################################################################
 # Menu                                                                        #
 ###############################################################################
 
-while :
-do
-    clear
-    cat<<EOF
-    ########################################################################
-    # Clean-macOS                                                          #
-    # Version : 1.12.21                                                    #
-    ########################################################################
-    #                                                                      #
-    #  Please enter your choice:                                           #
-    #                                                                      #
-    #  (1) Install                                                         #
-    #  (2) Configure                                                       #
-    #  (3) Update                                                          #
-    #  (0) Exit                                                            #
-    #                                                                      #
-    ########################################################################
+install() {
+  echo "Ready to install Homebrew..."
+  "${BIN}/install.sh"
+}
+
+configure() {
+  echo "Ready to configure macOS..."
+  "${BIN}/config.sh"
+}
+
+update() {
+  echo "Ready to update Homebrew..."
+  "${BIN}/update.sh"
+}
+
+while true; do
+  clear
+  cat <<EOF
+  ########################################################################
+  # Clean-macOS                                                          #
+  # Version : 1.13.0                                                     #
+  ########################################################################
+  #                                                                      #
+  #  Please enter your choice:                                           #
+  #                                                                      #
+  #  (1) Install                                                         #
+  #  (2) Configure                                                       #
+  #  (3) Update                                                          #
+  #  (0) Exit                                                            #
+  #                                                                      #
+  ########################################################################
 EOF
-    read -n1 -s
-    case "$REPLY" in
-    "1")  echo "Ready to install Homebrew..."            | $BIN/install.sh       ;;
-    "2")  echo "Ready to configure macOS..."             | $BIN/config.sh        ;;
-    "3")  echo "Ready to update Homebrew..."             | $BIN/update.sh        ;;
-    "0")  exit                                                                   ;;
-     * )  echo "Invalid option!"                                                 ;;
-    esac
-    sleep 1
+  read -n1 -s
+  case "$REPLY" in
+    1) install ;;
+    2) configure ;;
+    3) update ;;
+    0) exit ;;
+    *) echo "Invalid option!" ;;
+  esac
+  sleep 1
 done
